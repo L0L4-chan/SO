4bcfd309e43b3c14966963f7c4a85f31065942e2
L0L4-chan <d.suarez2@udc.es>
2023-10-12 10:23:39 +0200

    Create done!!!

--- p1.c
+++ p1.c
@@ -13,42 +13,36 @@ void ToCreate(char * command[], int com){
     }else if (com == 2) {
         if(!strcmp(command[1], "-f")){
             ChangeDir(command, 1);
-            return;
+        }else{
+            char location[256]; //to store the location
+            getcwd(location, sizeof(location));
+            strcpy(location, command[1]);
+            if(mkdir(location,S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH)==-1){
+                perror("It has not been possible to create the file\n");
+            }
+            printf("Directory %s created \n", command[1] );
+            }//https://man7.org/linux/man-pages/man2/mkdir.2.html
+    }else if(com == 3){
+        if(!strcmp(command[1], "-f")){
+            if(creat(command[2], S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)==-1){
+                perror("It has not been possible to create the directory\n");
+            }
+            printf("File %s created \n", command[2] );
         }
     }
-    char location[256]; //to store the location
-    getcwd(location, sizeof(location));
-    strcpy(location, command[1]);
-    int mode, i; //to review the permits
-    for (i = 2; command[i] != NULL; i++) {
-        if (!strcmp(command[i], "cr")) {
-            mode |= O_CREAT;
-        } else if (!strcmp(command[i], "ex")) {
-            mode |= O_EXCL;
-        } else if (!strcmp(command[i], "ro")) {
-            mode |= O_RDONLY;
-        } else if (!strcmp(command[i], "wo")) {
-            mode |= O_WRONLY;
-        } else if (!strcmp(command[i], "rw")) {
-            mode |= O_RDWR;
-        } else if (!strcmp(command[i], "ap")) {
-            mode |= O_APPEND;
-        } else if (!strcmp(command[i], "command")) {
-            mode |= O_TRUNC;
-        }
-    }
-    mkdir(location,mode);//https://man7.org/linux/man-pages/man2/mkdir.2.html
 }
 
+
+
 void ShowStat(){
-    printf("it works");
+    printf("it works\n");
 }
 void ToList(){
-    printf("it works");
+    printf("it works\n");
 }
 void ToDelete(){
-    printf("it works");
+    printf("it works\n");
 }
 void ToDeleteTree(){
-    printf("it works");
+    printf("it works\n");
 }
