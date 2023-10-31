f860b5988bae052a26be19005633942a847e0ae4
L0L4-chan <d.suarez2@udc.es>
2023-10-31 13:02:52 +0100

    headers added

--- p2.c
+++ p2.c
@@ -1,3 +1,27 @@
 //
 // Created by lola on 30/10/23.
 //
+
+#include <sys/stat.h>
+#include <dirent.h>
+#include "p2.h"
+#include "p1.h"
+#include "p0.h"
+#include "ayuda1.c"
+#include "ayuda2.c"
+
+void Make_Malloc(char * command[], int com){
+
+}
+void Make_Sharec(char * command[], int com){
+
+}
+void Make_Mmap(char * command [], int com){
+
+}
+void ToRead(char * command[], int com){
+
+}
+void ToWrite(char * command[], int com){
+    
+}
--- p2.h
+++ p2.h
@@ -3,6 +3,16 @@
 //
 
 #ifndef SO_P2_H
+
+#include "p2.c"
+
+
+void Make_Malloc(char * command[], int com);
+void Make_Sharec(char * command[], int com);
+void Make_Mmap(char * command [], int com);
+void ToRead(char * command[], int com);
+void ToWrite(char * command[], int com);
+
 #define SO_P2_H
 
 #endif //SO_P2_H
