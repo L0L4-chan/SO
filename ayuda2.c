0ad2f13cf3bdb30fe9d575f063af059fcba7b7c9
IsmaelMiguez <i.miguezv@udc.es>
2023-11-20 02:58:06 +0100

    actualización

--- ayuda2.c
+++ ayuda2.c
@@ -258,33 +258,33 @@ ssize_t EscribirFichero (char *f, void *p, size_t cont,int overwrite)
 
 
 
-//void Do_MemPmap (void) /*sin argumentos*/
- //{ pid_t pid;       /*hace el pmap (o equivalente) del proceso actual*/
- //  char elpid[32];
-//   char *argv[4]={"pmap",elpid,NULL};
+void Do_MemPmap (void) /*sin argumentos*/
+ { pid_t pid;       /*hace el pmap (o equivalente) del proceso actual*/
+   char elpid[32];
+   char *argv[4]={"pmap",elpid,NULL};
 
-//   sprintf (elpid,"%d", (int) getpid());
-//   if ((pid=fork())==-1){
-//      perror ("Imposible crear proceso");
-//      return;
- //     }
-//   if (pid==0){ /*proceso hijo*/
-//      if (execvp(argv[0],argv)==-1)
- //        perror("cannot execute pmap (linux, solaris)");
+   sprintf (elpid,"%d", (int) getpid());
+   if ((pid=fork())==-1){
+      perror ("Imposible crear proceso");
+      return;
+      }
+   if (pid==0){ /*proceso hijo*/
+      if (execvp(argv[0],argv)==-1)
+         perror("cannot execute pmap (linux, solaris)");
 
-//      argv[0]="vmmap"; argv[1]="-interleave"; argv[2]=elpid;argv[3]=NULL;
- //     if (execvp(argv[0],argv)==-1) /*probamos vmmap Mac-OS*/
-//         perror("cannot execute vmmap (Mac-OS)");
+      argv[0]="vmmap"; argv[1]="-interleave"; argv[2]=elpid;argv[3]=NULL;
+      if (execvp(argv[0],argv)==-1) /*probamos vmmap Mac-OS*/
+         perror("cannot execute vmmap (Mac-OS)");
 
-//      argv[0]="procstat"; argv[1]="vm"; argv[2]=elpid; argv[3]=NULL;
-//      if (execvp(argv[0],argv)==-1)/*No hay pmap, probamos procstat FreeBSD */
-//         perror("cannot execute procstat (FreeBSD)");
+      argv[0]="procstat"; argv[1]="vm"; argv[2]=elpid; argv[3]=NULL;
+      if (execvp(argv[0],argv)==-1)/*No hay pmap, probamos procstat FreeBSD */
+         perror("cannot execute procstat (FreeBSD)");
 
-//      argv[0]="procmap",argv[1]=elpid;argv[2]=NULL;
-//            if (execvp(argv[0],argv)==-1)  /*probamos procmap OpenBSD*/
-//         perror("cannot execute procmap (OpenBSD)");
+      argv[0]="procmap",argv[1]=elpid;argv[2]=NULL;
+            if (execvp(argv[0],argv)==-1)  /*probamos procmap OpenBSD*/
+         perror("cannot execute procmap (OpenBSD)");
 
-//      exit(1);
-//  }
-//  waitpid (pid,NULL,0);
-// }
+      exit(1);
+  }
+  waitpid (pid,NULL,0);
+ }
--- p2.c
+++ p2.c
@@ -4,9 +4,11 @@
 
 #include <sys/stat.h>
 #include <dirent.h>
+#include "p2.h"
+#include "p1.h"
 #include "p0.h"
+#include "ayuda1.c"
 #include "ayuda2.c"
-
 
 void Make_Malloc(char * command[], int com){
 //https://man7.org/linux/man-pages/man2/brk.2.html
@@ -17,17 +19,17 @@ void Make_Malloc(char * command[], int com){
         if (( increment = atoi (command[1])!=0)){
             void * ptr;
           if((ptr = malloc(increment))!=NULL){
-              tMemList * block = malloc(sizeof( tMemList));
-              block->addr = ptr;
-              block->type = "malloc";
-              block->size = increment;
+              tMemList block;
+              block.addr = ptr;
+              block.type = "malloc";
+              block.size = increment;
               time_t t = time(NULL);
               struct tm tiempoLocal = *localtime(&t);
               char date[20];
               char *formato = "%H:%M:%S";
               int datebytes = strftime(date, sizeof date, formato, &tiempoLocal);
               if (datebytes != 0) {
-                  block->date = date;
+                  block.date = date;
               } else {
                   perror("Output error\n");
               }
@@ -35,23 +37,45 @@ void Make_Malloc(char * command[], int com){
               printf("%d of memory reserved at %p\n", increment,ptr);
           }
             perror("Reserve of memory could not be done\n");
+       /* intptr_t increment = atoi(
+                command[1]); //obtenemos el tamaño a incrementar, pendiente comprobar que esto no da error
+        if ((f_address = sbrk(increment)) == -1) {
+            perror("We could not reserve memory ");
+        } else {
+
+        tMemList block;
+        block.addr = f_address;
+        block.type = "malloc";
+        block.size = increment;
+        time_t t = time(NULL);
+        struct tm tiempoLocal = *localtime(&t);
+        char date[20];
+        char *formato = "%H:%M:%S";
+        int datebytes = strftime(date, sizeof date, formato, &tiempoLocal);
+        if (datebytes != 0) {
+            block.date = date;
+        } else {
+            perror("Output error\n");
+        }
+        insertItem(block, memLog);
+    }*/
         }
     }else if (com ==  3){// free y el espacio a liberar
         if(!strcmp(command[1],"-free")){
 
-            tPos pos = first(memoryLog);
+            tPos pos = first(*memoryLog);
             while(pos!=NULL){
-                tMemList * aux = (tMemList *)getItem(pos, memoryLog);
+                tMemList * aux = (tMemList *)getItem(pos, *memoryLog);
                 int increment = atoi(
                         command[2]);
                 if(increment==aux->size){
                     insertItem(aux,fLog);
                     free(aux->addr);
                     printf("%d of memory free at %p\n", increment,aux->addr);
-                    deleteAtPosition(pos, memLog);
+                    deleteAtPosition(pos, memoryLog);
                     return;
                 }else{
-                pos = next(pos, memoryLog);
+                pos = next(pos, *memoryLog);
                 }
             }
                 perror("could not free memory");
@@ -61,41 +85,27 @@ void Make_Malloc(char * command[], int com){
 
 
 }
-void Make_Shared(char * command[], int com) {
+void Make_Shared(char * command[], int com){
 //https://man7.org/linux/man-pages/man2/shmget.2.html
 
-   if (com == 3) {  //create clave y tamaño
-        if (!strcmp(command[1], "-free")) {
-            tPos pos = first(memoryLog);
-            while (pos != NULL) {
-                tMemList *aux = (tMemList *) getItem(pos, memoryLog);
-                if (!strcmp(command[2], aux->key)) {
-                    printf("Shared memory at %p has been delete\n", aux->addr);
-                    deleteAtPosition(pos, memLog);
-                    return;
-                }
-                next(pos, memoryLog);
-            }
-        } else if (!strcmp(command[1], "-delkey")) {
-            tPos pos = first(memoryLog);
-            while (pos != NULL) {
-                tMemList *aux = (tMemList *) getItem(pos, memoryLog);
-                if (!strcmp(command[2], aux->key)) {
-                    printf("Shared memory with key %p has been delete form map\n", aux->key);
-                    free(aux->addr);
-                    return;
-                }
-                next(pos, memoryLog);
-            }
-        }
-    } else if (com == 4) {
-        SharedCreate(command);
-    }
-}
+    if (com == 2 ){ //free delkey  y la clave
 
+    }else if (com ==  3){  //create clave y tamaño
+
+    }else{
+
+    }
+
+
+}
 void Make_Mmap(char * command [], int com){
 //https://man7.org/linux/man-pages/man2/mmap.2.html
-    CmdMmap(command);
+
+    if (com == 3 ){// -free y nombre del fichero  sera mayor y la otra son el nombre del fichero y los permisos
+
+    }else{
+
+    }
 
 }
 void ToRead(char * command[], int com){
@@ -154,27 +164,94 @@ void ToWrite(char * command[], int com){
     }
 }
 
-
+//"memdump addr cont\tDump on screen contents (cont bytes) of memory position addr
 void Make_Memdump(char * command[], int com){
+    int cont;
+    if (com == 1)
+        return;
+    else if(com == 2){
+        cont = 25;
 
+    }
+    else{
+        cont = (int) command[2];
+
+
+    }
 
 }
 
 
 void Make_Memfill(char * command[], int com){
-
-
+    size_t tam = (size_t) command[2];
+    unsigned char bytefill = (unsigned char) command[3];
+    if (com==1)
+        return;
+    else {
+        if (com==2)
+            LlenarMemoria(command[1], 25, 41);
+        else if (com == 3)
+            LlenarMemoria(command[1], tam, 41);
+        else
+            LlenarMemoria(command[1], tam, bytefill);
+    }
 }
 
-
+//"mem [-blocks|-funcs|-vars|all|-pmap]...\tShows processs memory detalils\n"
+//                   "\t-blocks: memory blocks allocated\n"
+//                   "\t-funcs: function adresses\n"
+//                   "\t-vars: variable addresses\n"
+//                   "\t-all: all information available\n"
+//                   "\t-pmap: shows the pmap command output (or similar)\n");
 void ToMem(char * command[], int com){
+    bool blocks = false;
+    bool funcs = false;
+    bool vars = false;
+    bool pmap = false;
 
+    for(int i = 1; i<com; i++){
+        if(!strcmp(command[i], "-blocks")&& !blocks){
+            blocks=true;
+        }
+        if(!strcmp(command[i], "-funcs")&& !funcs){
+            funcs=true;
+        }
+        if( !strcmp(command[i], "-vars")&& !vars){
+            vars=true;
+        }
+        if( !strcmp(command[i], "-pmap")&& !pmap){
+            pmap=true;
+        }
+        if( !strcmp(command[i], "-all")){
+            blocks=true;
+            funcs=true;
+            vars=true;
+        }
+    }
 
+    if (blocks){
+
+    }
+    if (funcs){
+
+    }
+    if (vars){
+
+    }
+    if (pmap){
+        Do_MemPmap();
+    }
 }
 
 
 void ToRecurse(char * command[], int com){
-
-
+    if (com == 1)
+        return;
+    else if (com > 1){
+        if (command[1]>=0)
+            Recursiva((int) command[1]);
+        else
+            Recursiva(0);
+    }
 }
 
