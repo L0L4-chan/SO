420a117e0046aee645036115c20e8a51ca48f3a7
L0L4-chan <d.suarez2@udc.es>
2023-11-20 09:40:26 +0100

    fixed untested

---
+++ Makefile
@@ -0,0 +1,2 @@
+p2: p0.c p0.h ayuda1.c ayuda1.h p1.c p1.h ayuda2.c ayuda2.h p2.c p2.h
+	gcc -Wall -g -o p2 p0.c
---
+++ ayuda2.h
@@ -0,0 +1,12 @@
+//
+// Created by lola on 19/11/23.
+//
+
+#ifndef SO_AYUDA2_H
+#define SO_AYUDA2_H
+#include "ayuda2.h"
+
+void ImprimirListaMmap();
+
+
+#endif //SO_AYUDA2_H
